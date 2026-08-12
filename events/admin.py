b3a0f1ecdebb761d5f0e5b15a01313e89bdcd3c9
sanjina-stha <sanjina.aa@yahoo.com>
2026-08-12 12:31:52 +0545

    changes made by sanjina

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Event, Booking
 
 # Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'location', 'capacity', 'created_at')
+    search_fields = ('title', 'location')
+    list_filter = ('date',)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event', 'booked_at')
+    search_fields = ('user__username', 'event__title')
+    list_filter = ('booked_at',)
